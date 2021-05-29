95f58cf670e154f7111634296e2246fd40723789
Clifton Craig <clifton.craig@gmail.com>
2021-05-29 15:18:33 -0700

    Adds Todo component to app.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Todos } from './Todos';
 
 function App() {
   return (
@@ -7,17 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Todos App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Todos/>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {Todos} from './Todos';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Our application',() => {
+  it('Should render our Todos component', () => {
+    const app = shallow(<App />);
+    expect(app.find(Todos).length).toBeGreaterThanOrEqual(1)
+  });
 });
