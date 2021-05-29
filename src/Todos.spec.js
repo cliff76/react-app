5159422a4fd27c78937c78cb3564a3b400255602
Clifton Craig <clifton.craig@gmail.com>
2021-05-29 15:18:33 -0700

    refactors first todo test spec

--- src/Todos.spec.js
+++ src/Todos.spec.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import { Todos } from './Todos';
 
-test('renders todos component', () => {
-  render(<Todos />);
-  const linkElement = screen.getByText(/Todo List/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Our Todos component', () => {
+  beforeEach(() => {
+    render(<Todos />);
+  });
+
+  it('renders a todos title', () => {
+    const expectedText = screen.getByText(/Todo List/i);
+    expect(expectedText).toBeInTheDocument();
+  });
 });
