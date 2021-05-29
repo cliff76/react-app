e48016e16cd04750d35df240152697c2abe6f095
Clifton Craig <c.craig@samsung.com>
2021-05-29 15:17:00 -0700

    Extracts TodoInput component.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 import React from "react";
 
 
+function TodoInput() {
+  return <div>
+    <label className="Form-component" htmlFor="addTodo">
+      New To-Do:
+      <input className="Form-component" name="addTodo"/>
+    </label>
+    <input type="submit" value="Add Todo"/>
+  </div>;
+}
+
 function App() {
 
   const [todos, setTodos] = React.useState([])
@@ -10,25 +20,23 @@ function App() {
   return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <form onSubmit={event => {
             event.preventDefault()
             setTodos(todos => [...todos, event.target.addTodo.value])
           }}>
-            <label className="Form-component" htmlFor='addTodo'>
-              New To-Do:
-              <input className="Form-component" name='addTodo'/>
-            </label>
-            <input type='submit' value='Add Todo' />
+            <TodoInput/>
           </form>
-          <p>
-            My To-Dos
-          </p>
-          {
-            todos.map((each, index) => {
-              return ( <div key={index}>{each}</div> )
-            })
-          }
+          <div>
+            <p>
+              My To-Dos
+            </p>
+            {
+              todos.map((each, index) => {
+                return (<div key={index}>{each}</div>)
+              })
+            }
+          </div>
         </header>
       </div>
   );
